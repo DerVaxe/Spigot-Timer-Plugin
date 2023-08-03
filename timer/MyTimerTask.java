b390383c261ac5daa35f7c531e25e527237483dd
DerVaxe <83669969+DerVaxe@users.noreply.github.com>
2023-08-03 16:06:50 +0200

    Update MyTimerTask.java

--- timer/MyTimerTask.java
+++ timer/MyTimerTask.java
@@ -29,37 +29,36 @@ public class MyTimerTask extends BukkitRunnable {
     @Override
     public void run() {
         time++;
+        int hours = time / 3600;
+        int minutes = (time % 3600) / 60;
+        int seconds = time% 60;
+        String timeString;
+
+        if(show_units){
+            if(hours >= 1 || show_instant){
+                timeString = String.format("%02dh %02dm %02ds", hours, minutes, seconds);
+            }
+            else if(minutes >= 1){
+                timeString = String.format("%02dm %02ds", minutes, seconds);
+            }else{
+                timeString = String.format("%02ds", seconds);
+            }
+        }
+
+        else{
+            if(hours >= 1|| show_instant){
+                timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+            }
+            else if(minutes >= 1){
+                timeString = String.format("%02d:%02d", minutes, seconds);
+            }else{
+                timeString = String.format("%02d", seconds);
+            }
+        }
+
         for(Player p : Bukkit.getOnlinePlayers()){
-            int hours = time / 3600;
-            int minutes = (time % 3600) / 60;
-            int seconds = time% 60;
-            String timeString;
-
-            if(show_units){
-                if(hours >= 1 || show_instant){
-                    timeString = String.format("%02dh %02dm %02ds", hours, minutes, seconds);
-                }
-                else if(minutes >= 1){
-                    timeString = String.format("%02dm %02ds", minutes, seconds);
-                }else{
-                    timeString = String.format("%02ds", seconds);
-                }
-            }
-
-            else{
-                if(hours >= 1|| show_instant){
-                    timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-                }
-                else if(minutes >= 1){
-                    timeString = String.format("%02d:%02d", minutes, seconds);
-                }else{
-                    timeString = String.format("%02d", seconds);
-                }
-            }
-
-
-
             actionBar.sendMessage(p, plugin.getConfig().getString("timer-color")+timeString);
         }
+        
     }
 }
